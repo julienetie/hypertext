998c3fbd2070f9031fd721925f926974e60bdbb2
julienetie <j8@protonmail.com>
2016-10-23 06:36:24 +0100

    Cleaned up create.js

--- src/create-nodes/create.js
+++ src/create-nodes/create.js
@@ -3,19 +3,18 @@ import storeEventTarget from '../events/store-event-target';
 import applyProperties from './apply-properties';
 
 export default function createNodes(virtualNode, opts) {
-    const doc = opts ? opts.document || document : document;
     const warn = opts ? opts.warn : null;
-    let i;
-    let node;
-    let children;
-    let childNode;
     let vnode = virtualNode;
     let virtualNodeEvent = virtualNode.event;
+    let children;
+    let childNode;
+    let node;
 
+    // If widget, Vtext or not a vnode
     if (isWidget(vnode)) {
         return vnode.init();
     } else if (isVirtualText(vnode)) {
-        return doc.createTextNode(vnode.text);
+        return document.createTextNode(vnode.text);
     } else if (!isVirtualNode(vnode)) {
         if (warn) {
             warn("Item is not a valid virtual dom node", vnode);
@@ -23,20 +22,22 @@ export default function createNodes(virtualNode, opts) {
         return null;
     }
 
+    // if no namespace create HTML, else create SVG
     if (vnode.namespace === null) {
-        node = doc.createElement(vnode.tagName);
+        node = document.createElement(vnode.tagName);
         if(virtualNodeEvent){
             storeEventTarget(node, virtualNodeEvent);
         }
     } else {
-        node = doc.createElementNS(vnode.namespace, vnode.tagName);
+        node = document.createElementNS(vnode.namespace, vnode.tagName);
     }
 
     applyProperties(node, vnode.properties);
 
     children = vnode.children;
+    let childrenLength = children.length;
 
-    for (i = 0; i < children.length; i++) {
+    for (let i = 0; i < childrenLength; i++) {
         childNode = createNodes(children[i], opts);
         if (childNode) {
             node.appendChild(childNode);
