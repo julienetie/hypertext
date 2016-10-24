8272c709c2ee7bd8bd0e2759e6a17dbb3198abb3
julienetie <j8@protonmail.com>
2016-10-24 16:56:49 +0100

    Resolved working createVTree implementation

--- demo/app.js
+++ demo/app.js
@@ -1,116 +1,125 @@
-// // var layout = createNodes(
-// var tree = 
-// div({class:'main-wrapper'},
 
-//   div({class:'hero-image'},
-//     div({class:'hero-content'},
-//       div({class:'hc-label'},'The Italian Dolomites'),
-//       div({class:'hc-desc'},'Interview with Andy Free about his trip to the dolomites in Italy'),
-//       div({class:'hc-footer'},
-//         div({class:'hcf-comments'},span({class:'amount'},'168'),'Comments'),
-//         div({class:'hcf-divider'}),
-//         div({class:'hcf-shares'},span({class:'amount'},'26.842'),'Shares'),
-//         div({class:'hcf-toggle'},
-//           p({class:'hcft-label'},'More Articles'),
-//           div({class:'hcft-icon-holder'},
-//             i({class:'hcft-icon closed fa fa-chevron-down'}),
-//             i({class:'hcft-icon opened fa fa-times'})
-//           )
-//         )
-//       )
-//     )
-//   ),
+var tree = 
+div({class:'main-wrapper'},
+
+  div({class:'hero-image'},
+    div({class:'hero-content'},
+      div({class:'hc-label'},'The Italian Dolomites'),
+      div({class:'hc-desc'},'Interview with Andy Free about his trip to the dolomites in Italy'),
+      div({class:'hc-footer'},
+        div({class:'hcf-comments'},span({class:'amount'},'168'),'Comments'),
+        div({class:'hcf-divider'}),
+        div({class:'hcf-shares'},span({class:'amount'},'26.842'),'Shares'),
+        div({class:'hcf-toggle'},
+          p({class:'hcft-label'},'More Articles'),
+          div({class:'hcft-icon-holder'},
+            i({class:'hcft-icon closed fa fa-chevron-down'}),
+            i({class:'hcft-icon opened fa fa-times'})
+          )
+        )
+      )
+    )
+  ),
  
-//   div({class:'scroll-content'},
-//     div({class:'sc-row'},
-//       div({class:'content-article'},
-//         div({class:'article-number'},'01'),
-//         div({class:'article-info'},
-//           div({class:'ai-label'},'Visiting Vester Peak'),
-//           div({class:'ai-desc'},'Everything you should know about hiking in the mountains'),
-//           div({class:'ai-footer'},
-//             div({class:'aif-comments'},span({class:'amount'},'24'),'Comments'),
-//             div({class:'aif-divider'}),
-//             div({class:'aif-shares'},span({class:'amount'},'128'),'Shares')
-//           )
-//         ),
-//         div({class:'article-image ca-1'})
-//       ),
-//       div({class:'content-article'},
-//         div({class:'article-number'},'02'),
-//         div({class:'article-info'},
-//           div({class:'ai-label'},'Trailing Together'),
-//           div({class:'ai-desc'},'Walking among the nature in the Rocky Mountain National Park, Colorado'),
-//           div({class:'ai-footer'},
-//             div({class:'aif-comments'},span({class:'amount'},'458'),'Comments'),
-//             div({class:'aif-divider'}),
-//             div({class:'aif-shares'},span({class:'amount'},'110'),'Shares')
-//           )
-//         ),
-//         div({class:'article-image ca-2'})
-//       )
-//     ),
-//     div({class:'sc-row'},
-//       div({class:'content-article'},
-//         div({class:'article-number'},'03'),
-//         div({class:'article-info'},
-//           div({class:'ai-label'},'The Magic Pond'),
-//           div({class:'ai-desc'},'The scenery is set like a Monet painting'),
-//           div({class:'ai-footer'},
-//             div({class:'aif-comments'},span({class:'amount'},1640),'Comments'),
-//             div({class:'aif-divider'}),
-//             div({class:'aif-shares'},span({class:'amount'},86.451),'Shares')
-//           )
-//         ),
-//         div({class:'article-image ca-3'})
-//       ),
-//       div({class:'content-article'},
-//         div({class:'article-number'},'04'),
-//         div({class:'article-info'},
-//           div({class:'ai-label'},'Beach Essentials'),
-//           div({class:'ai-desc'},'What should you bring to the beach, and what beaches should you visit?'),
-//           div({class:'ai-footer'},
-//             div({class:'aif-comments'},span({class:'amount'},58),'Comments'),
-//             div({class:'aif-divider'}),
-//             div({class:'aif-shares'},span({class:'amount'},1.359),'Shares')
-//           )
-//         ),
-//         div({class:'article-image ca-4'})
-//       )
-//     ),
-//     div({class:'sc-row'},
-//       div({class:'content-article'},
-//         div({class:'article-number'},'05'),
-//         div({class:'article-info'},
-//           div({class:'ai-label'},'Romantic Camping'),
-//           div({class:'ai-desc'},'We went to Norway to find out just how romantic it can be sleeping under the stars'),
-//           div({class:'ai-footer'},
-//             div({class:'aif-comments'},span({class:'amount'},14),'Comments'),
-//             div({class:'aif-divider'}),
-//             div({class:'aif-shares'},span({class:'amount'},36),'Shares')
-//           )
-//         ),
-//         div({class:'article-image ca-5'})
-//       ),
-//       div({class:'content-article'},
-//         div({class:'article-number'},'06'),
-//         div({class:'article-info'},
-//           div({class:'ai-label'},'Cave Explorers'),
-//           div({class:'ai-desc'},'In USA there\'s over a thousand caves. We list everyone that you should visit.'),
-//           div({class:'ai-footer'},
-//             div({class:'aif-comments'},span({class:'amount'},3590),'Comments'),
-//             div({class:'aif-divider'}),
-//             div({class:'aif-shares'},span({class:'amount'},129.547),'Shares')
-//           )
-//         ),
-//         div({class:'article-image ca-6'})
-//       )
-//     )
-//   )
-// )
-// // );
-// // document.body.appendChild(layout)
+  div({class:'scroll-content'},
+    div({class:'sc-row'},
+      div({class:'content-article'},
+        div({class:'article-number'},'01'),
+        div({class:'article-info'},
+          div({class:'ai-label'},'Visiting Vester Peak'),
+          div({class:'ai-desc'},'Everything you should know about hiking in the mountains'),
+          div({class:'ai-footer'},
+            div({class:'aif-comments'},span({class:'amount'},'24'),'Comments'),
+            div({class:'aif-divider'}),
+            div({class:'aif-shares'},span({class:'amount'},'128'),'Shares')
+          )
+        ),
+        div({class:'article-image ca-1'})
+      ),
+      div({class:'content-article'},
+        div({class:'article-number'},'02'),
+        div({class:'article-info'},
+          div({class:'ai-label'},'Trailing Together'),
+          div({class:'ai-desc'},'Walking among the nature in the Rocky Mountain National Park, Colorado'),
+          div({class:'ai-footer'},
+            div({class:'aif-comments'},span({class:'amount'},'458'),'Comments'),
+            div({class:'aif-divider'}),
+            div({class:'aif-shares'},span({class:'amount'},'110'),'Shares')
+          )
+        ),
+        div({class:'article-image ca-2'})
+      )
+    ),
+    div({class:'sc-row'},
+      div({class:'content-article'},
+        div({class:'article-number'},'03'),
+        div({class:'article-info'},
+          div({class:'ai-label'},'The Magic Pond'),
+          div({class:'ai-desc'},'The scenery is set like a Monet painting'),
+          div({class:'ai-footer'},
+            div({class:'aif-comments'},span({class:'amount'},1640),'Comments'),
+            div({class:'aif-divider'}),
+            div({class:'aif-shares'},span({class:'amount'},86.451),'Shares')
+          )
+        ),
+        div({class:'article-image ca-3'})
+      ),
+      div({class:'content-article'},
+        div({class:'article-number'},'04'),
+        div({class:'article-info'},
+          div({class:'ai-label'},'Beach Essentials'),
+          div({class:'ai-desc'},'What should you bring to the beach, and what beaches should you visit?'),
+          div({class:'ai-footer'},
+            div({class:'aif-comments'},span({class:'amount'},58),'Comments'),
+            div({class:'aif-divider'}),
+            div({class:'aif-shares'},span({class:'amount'},1.359),'Shares')
+          )
+        ),
+        div({class:'article-image ca-4'})
+      )
+    ),
+    div({class:'sc-row'},
+      div({class:'content-article'},
+        div({class:'article-number'},'05'),
+        div({class:'article-info'},
+          div({class:'ai-label'},'Romantic Camping'),
+          div({class:'ai-desc'},'We went to Norway to find out just how romantic it can be sleeping under the stars'),
+          div({class:'ai-footer'},
+            div({class:'aif-comments'},span({class:'amount'},14),'Comments'),
+            div({class:'aif-divider'}),
+            div({class:'aif-shares'},span({class:'amount'},36),'Shares')
+          )
+        ),
+        div({class:'article-image ca-5'})
+      ),
+      div({class:'content-article'},
+        div({class:'article-number'},'06'),
+        div({class:'article-info'},
+          div({class:'ai-label'},'Cave Explorers'),
+          div({class:'ai-desc'},'In USA there\'s over a thousand caves. We list everyone that you should visit.'),
+          div({class:'ai-footer'},
+            div({class:'aif-comments'},span({class:'amount'},3590),'Comments'),
+            div({class:'aif-divider'}),
+            div({class:'aif-shares'},span({class:'amount'},129.547),'Shares')
+          )
+        ),
+        div({class:'article-image ca-6'})
+      )
+    )
+  )
+)
+console.log(tree)
+
+// var layout = createNodes(tree);
+// console.log(layout)
+// document.body.appendChild(layout)
+
 // console.log(tree)
+
+
+
+
+
 const version = '2';
 
 const isThunk = (thunk) => {
@@ -191,13 +200,14 @@ function VirtualNode(propertiesVal, childrenVal, keyVal, namespaceVal) {
             hasThunks = true;
         }
     }
-
+        
     return {
       count:  count + descendants || 0,
       hasWidgets,
       hasThunks,
       hooks,
-      descendantHooks
+      descendantHooks,
+      key
     }
 }
 
@@ -225,7 +235,7 @@ const createVTree = (interfaceSelector, whitespaceRules = 'trim') => {
     let attribute;
     let attributeName;
     let namespace = false;
-    let key = false;
+    let key = null;
     let event = false;
 
     for(let i = 0; i < attributesLength; i++){
@@ -300,16 +310,23 @@ const createVTree = (interfaceSelector, whitespaceRules = 'trim') => {
   }
 
 
-  if(element.childNodes.length){
-    // Children.
-    vTree.children = getChildNodesAsArray(element.childNodes); 
-  }
+
+
 
 
   if(element.nodeType === 3){
   // Text.
     vTree.text = element.nodeValue;
+    vTree.version = '2';
+    vTree.type = "VirtualText";
   }else{
+
+  if(element.childNodes.length){
+    // Children.
+    vTree.children = getChildNodesAsArray(element.childNodes); 
+  }else{
+    vTree.children = [];
+  }
 
       const definedAttributes = getDefinedAttributes(element.attributes);
 
@@ -319,7 +336,7 @@ const createVTree = (interfaceSelector, whitespaceRules = 'trim') => {
       vTree.properties = definedAttributes.attr;
       
       // Namespace.
-      vTree.namespace = !!definedAttributes.namespace;
+      vTree.namespace = definedAttributes.namespace ? true : null;
 
       // Event.
       vTree.event = !!definedAttributes.event;  
@@ -343,6 +360,13 @@ const createVTree = (interfaceSelector, whitespaceRules = 'trim') => {
 
       vTree.descendantHooks = virtualNode.descendantHooks;
 
+      vTree.hooks = virtualNode.hooks;
+
+      vTree.key = virtualNode.key;
+
+
+      vTree.version = '2';
+      vTree.type = "VirtualNode";
     }
 
 
@@ -354,4 +378,9 @@ const createVTree = (interfaceSelector, whitespaceRules = 'trim') => {
   return vTree;
 }
 
-console.log(createVTree('.main-wrapper'));
+
+var section = document.getElementsByTagName('section')[0];
+var html = section.firstElementChild;
+var nodess =  createVTree(html)
+  console.log()
+document.body.appendChild(createNodes(nodess))
