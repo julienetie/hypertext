0f4b963f1dd34242d7956420b9cb22ff74657769
julienetie <j8@protonmail.com>
2016-10-31 13:36:19 +0000

    Removed test example
    Passed test except key property test and commented out test

--- test/assembly/assembly.js
+++ test/assembly/assembly.js
@@ -1,53 +1,80 @@
 import { assembly } from '../../src/assembly';
+const div = assembly('div');
+
+function VirtualText(text) {
+	this.text = text + '';
+}
+// VirtualText.prototype.version = '2';
+// VirtualText.prototype.type = "VirtualText";
+
+
+const divWithKey = div({ class: 'some-button', key: '12345' });
+const divWithEvent = div({ class: 'some-button', event: 'click-button' });
 
 describe('assembly', () => {
-	
+
 	it('Should be a function', () => {
 		expect(assembly).to.be.a('function');
 	});
 
 	it('Should uppercase tagName', () => {
-		expect(true).to.equal(false);
+		expect(div().tagName).to.equal('DIV');
 	});
 
 	it('Should return a virtual text node for a given string', () => {
-		expect(true).to.equal(false);
+		const testString = 'Hello World!';
+		const vText = new VirtualText(testString).text;
+		const virtualNode = div(testString).children[0].text;
+		expect(virtualNode).to.equal(vText);
 	});
 
 	it('Should return a virtual text node for a given number', () => {
-		expect(true).to.equal(false);
+		const testNumber = 12345;
+		const vText = new VirtualText(testNumber).text;
+		const virtualNode = div(testNumber).children[0].text;
+		expect(virtualNode).to.equal(vText);
 	});
 
 	it('Should return a virtual node with property "virtualNode"', () => {
-		expect(true).to.equal(false);
+
+		expect(div()).to.have.property('virtualNode')
 	});
 
-	it('Should reurn a virtual node without an event property when supplied an event', () => {
-		expect(true).to.equal(false);
+	it('Should reurn a virtual node without "properties" event property, when supplied an event', () => {
+		expect(divWithEvent.properties).to.not.have.property('event');
 	});
 
-	it('Should not throw an error for empty attribute objects', () => {
-		expect(true).to.equal(false);
+	it('Should reurn a virtual node with an event property when supplied an event', () => {
+		expect(divWithEvent).to.have.property('event');
 	});
 
-	it('Should not throw an error for empty attribute objects', () => {
-		expect(true).to.equal(false);
+	it('Should produce a virtual node when an empty attribute object is supplied', () => {
+		expect(div({})).to.be.ok;
 	});
+
 
 	it('Should return a virtual node with children for a given array of virtual nodes', () => {
-		expect(true).to.equal(false);
+		const parentWithChildren = div([div('Some text'), div(), div()]);
+		expect(parentWithChildren.children).to.be.an('array');
+		parentWithChildren.children.forEach((childVirtualNode) => {
+			expect(childVirtualNode).to.have.property('virtualNode');
+		});
 	});
 
-	it('Should reurn a virtual node without a key property when supplied an key', () => {
-		expect(true).to.equal(false);
+	it('Should reurn a virtual node without "properties" key property, when supplied a key', () => {
+		expect(divWithKey.properties).to.not.have.property('key');
 	});
 
-	it('Should reurn a virtual node with the supplied namespace', () => {
-		expect(true).to.equal(false);
+	it('Should reurn a virtual node with a key property when supplied a key', () => {
+		expect(divWithKey).to.have.property('key');
 	});
 
-	it('Should treat all objects containing "virtualNode" property as children', () => {
-		expect(true).to.equal(false);
-	});
-	
+	// it('Should reurn a virtual node with the supplied namespace', () => {
+	// 	expect(true).to.equal(false);
+	// });
+
+	// it('Should treat all objects containing "virtualNode" property as children', () => {
+	// 	expect(true).to.equal(false);
+	// });
+
 });
